7dfb79505a90e126ea9b44923ee720c65ce506c9
Radu Mogos <radu.mogos@pixelplant.ch>
2015-08-02 22:22:56 +0200

    Code cleanup

--- Extensions/Local/News/Resources/Private/Frontend/Wizards/Plugins/show.wizard.php
+++ Extensions/Local/News/Resources/Private/Frontend/Wizards/Plugins/show.wizard.php
@@ -5,3 +5,6 @@
 <div class="form-group">
 	<?= $this->helper("Wizard")->textField("title", $this->__("backend.wizard.title"), $title) ?>
 </div>
+<div class="form-group">
+	<?= $this->helper("Wizard")->textField("limit", "How many news to show", $limit) ?>
+</div>
--- Extensions/System/Backend/Classes/Domain/Model/Content/BackendContainerContent.php
+++ Extensions/System/Backend/Classes/Domain/Model/Content/BackendContainerContent.php
@@ -44,6 +44,71 @@ namespace Extensions\System\Backend\Classes\Domain\Model\Content {
 
 			return $this->formatBlock("container", $title, $value);
 		}
+
+
+		/**
+		 * Renders the backend editable part of a content element
+		 *
+		 * @param string $type    The type of content element we're formating (deprecated, will be removed)
+		 * @param string $title   The title of the content element, if any
+		 * @param string $content The content of the element
+		 *
+		 * @return string
+		 */
+		protected function formatBlock($type, $title, $content) {
+			$linkToEdit   = sprintf('<a title="%s" class="btn btn-default content-operation-link" href="%s"><i class="fa fa-pencil fa-fw"></i></a>',
+				Utility::helper("Localization")->translate("backend.content.operation.edit"),
+				Utility::helper("Url")->linkToAction("Backend", "Content", "edit", ["uid" => $this->getUid()])
+			);
+
+			$linkToDelete = sprintf('<a title="%s" class="content-operation-link" href="%s"><i class="fa fa-trash-o fa-fw"></i> %s</a>',
+				Utility::helper("Localization")->translate("backend.content.operation.delete"),
+				Utility::helper("Url")->linkToAction("Backend", "Content", "delete", ["uid" => $this->getUid()]),
+				Utility::helper("Localization")->translate("backend.content.operation.delete")
+			);
+
+			$linkToCopy   = sprintf('<a title="%s" class="content-operation-link" href="%s"><i class="fa fa-copy fa-fw"></i> %s</a>',
+				Utility::helper("Localization")->translate("backend.content.operation.copy"),
+				Utility::helper("Url")->linkToAction("Backend", "Content", "copy", ["uid" => $this->getUid()]),
+				Utility::helper("Localization")->translate("backend.content.operation.copy")
+			);
+
+			$linkToHide   = sprintf('<a title="%s" class="content-operation-link" href="%s"><i class="fa fa-eye fa-fw"></i> %s</a>',
+				Utility::helper("Localization")->translate("backend.content.operation.hide"),
+				Utility::helper("Url")->linkToAction("Backend", "Content", "toggleVisibility", ["uid" => $this->getUid(), "show" => 0]),
+				Utility::helper("Localization")->translate("backend.content.operation.hide")
+			);
+
+			$linkToShow   = sprintf('<a title="%s" class="content-operation-link" href="%s"><i class="fa fa-eye-slash text-danger fa-fw"></i></a>',
+				Utility::helper("Localization")->translate("backend.content.operation.show"),
+				Utility::helper("Url")->linkToAction("Backend", "Content", "toggleVisibility", ["uid" => $this->getUid(), "show" => 1])
+			);
+
+			$linkNewElement = sprintf('<a class="btn btn-sm btn-success content-wizard" title="%s" href="%s"><i class="fa fa-plus fa-fw"></i></a>',
+				Utility::helper("Localization")->translate("backend.content.addNew"),
+				Utility::helper("Url")->linkToAction("Backend", "Content", "addElement", ["uid" => $this->getUid()])
+			);
+
+			if ($this->getIsVisible()) {
+				$visibilityLink = $linkToHide;
+				$visibilityClass = "panel-visible";
+			} else {
+				$visibilityLink = $linkToShow;
+				$visibilityClass = "panel-hidden";
+				$title .= Utility::helper("Localization")->translate("backend.content.headerIsHidden");
+			}
+
+			$operationLinks = sprintf('<div class="btn-group btn-group-sm pull-right no-pep" role="group" aria-label="Element actions">%s %s<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><span class="caret"></span><span class="sr-only">Toggle Dropdown</span></button><ul class="dropdown-menu" role="menu"><li>%s</li><li>%s</li><li>%s</li></div>',
+				$linkNewElement,
+				$linkToEdit,
+				$linkToCopy,
+				$visibilityLink,
+				$linkToDelete);
+
+			$overallWrap = '<div id="panel-backend-content-%s content-type-%s" data-id="%s" class="panel panel-backend-content content-drag-sender %s"><div class="panel-heading"><strong>%s</strong>%s</div><div class="panel-body no-pep">%s</div></div>';
+
+			return sprintf($overallWrap, $this->getUid(), $this->getType(), $this->getUid(), $visibilityClass, $title, $operationLinks, $content);
+		}
 	}
 
 }
